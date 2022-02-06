eb034af0d07dd09cafc6bfdd18d8ab03a6f26d49
boostup <boostup@gmail.com>
2022-02-06 08:44:41 +0100

    added a home link (available on all pages)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import Link from "next/link";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Link href={"/"}>Home</Link>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
