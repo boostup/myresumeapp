5b104a5fd994ca8de43ff70c65fd53b8a526076c
boostup <boostup@gmail.com>
2022-02-10 20:46:52 +0100

    refactoring

--- pages/resumeEditor.js
+++ pages/resumeEditor.js
@@ -1,13 +1,5 @@
 import Editor from "../components/Editor";
 
-import styles from "../styles/ResumeEditor.module.css";
-
 export default function ResumeEditor() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.main}>
-        <Editor />
-      </div>
-    </div>
-  );
+  return <Editor />;
 }
