3936d2e526c7b2d74be3d104b188a0913d7fcc4b
boostup <boostup@gmail.com>
2022-02-11 11:03:57 +0100

    custom mui filled input text

--- components/Editor/EditorForm/FilledTextInput.jsx
+++ components/Editor/EditorForm/FilledTextInput.jsx
@@ -1,8 +1,34 @@
-import { TextField } from "@mui/material";
+import { styled, TextField } from "@mui/material";
 
-function FilledTextInput(props) {
-  console.log(props);
-  return <TextField {...props} placeholder="Filled" variant="filled" />;
-}
+const backgroundColor = "rgb(239, 242, 249)";
+
+const FilledTextInput = styled((props) => (
+  <TextField variant="filled" {...props} />
+))(({ theme }) => ({
+  "& .MuiInputBase-input": {
+    // backgroundColor,
+    padding: "12px 16px",
+  },
+  "& .MuiFilledInput-root": {
+    backgroundColor,
+  },
+  "& .MuiFilledInput-root:before": {
+    borderBottom: "0",
+  },
+  "&:hover": {
+    backgroundColor,
+  },
+  "&.Mui-focused": {
+    backgroundColor,
+  },
+
+  "& .MuiFilledInput-root:hover": {
+    borderBottom: "0",
+  },
+
+  "& .MuiFilledInput-root.MuiFilledInput-underline:hover:before": {
+    borderBottom: "0",
+  },
+}));
 
 export default FilledTextInput;
