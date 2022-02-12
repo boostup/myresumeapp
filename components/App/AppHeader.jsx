af971cc87f2c732e4f9ac353306c009e6c8f7d81
boostup <boostup@gmail.com>
2022-02-12 12:53:11 +0100

    removed unused code

--- components/App/AppHeader.jsx
+++ components/App/AppHeader.jsx
@@ -3,25 +3,18 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
+import Link from "next/link";
+import { Button } from "@mui/material";
 // import MenuIcon from "@mui/icons-material/Menu";
 
 function AppHeader() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            {/* <MenuIcon /> */}
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Resume.cv
-          </Typography>
+        <Toolbar sx={{ px: { xs: 0, sm: 2 } }}>
+          <Link href="/">
+            <Button style={{ color: "white" }}>Resume.cv</Button>
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>
