a397964e5098189d138b586859d125d589d54196
boostup <boostup@gmail.com>
2022-02-12 12:55:55 +0100

    some minor tweaks

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,31 @@
-import { Button } from "@mui/material";
+import { Button, Container, Stack, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import Link from "next/link";
 
 import AppHeader from "../components/App/AppHeader";
 
 export default function Home() {
   return (
-    <div>
+    <Box>
       <AppHeader />
-      <div>
-        <Link href="/resumeEditor">
-          <Button>Edit Your CV</Button>
-        </Link>
-        <Link href="/sunEditor">
-          <Button>Test Sun Editor</Button>
-        </Link>
-      </div>
-    </div>
+      <Container>
+        <Box>
+          <Typography p={{ xs: 2, md: 4 }}>
+            This is the page where non logged-in visitors land on.
+          </Typography>
+
+          <Stack>
+            <Link href="/home">
+              <Button variant="contained">Log In (Simulated for now)</Button>
+            </Link>
+            <Link href="/sunEditor">
+              <Button variant="" style={{ justifyContent: "flex-start" }}>
+                Test Sun Editor
+              </Button>
+            </Link>
+          </Stack>
+        </Box>
+      </Container>
+    </Box>
   );
 }
