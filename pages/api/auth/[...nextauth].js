a224ef1e17ce2da9eb33752abbe054df2e2fa4ce
boostup <boostup@gmail.com>
2022-02-14 12:36:45 +0100

    added user id from oAuth provider to session obj

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-import { redirect } from "next/dist/server/api-utils";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -19,5 +18,12 @@ export default NextAuth({
     colorScheme: "light", // "auto" | "dark" | "light"
     brandColor: "#2f8deb", // Hex color code
     logo: "https://s3.resume.io/uploads/country/logo_default/2/for-light-bg.svg", // Absolute URL to image
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      console.log(session, token, user);
+      session.user.id = token.sub;
+      return session;
+    },
   },
 });
